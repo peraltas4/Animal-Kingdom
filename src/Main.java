7377572bd867006d5c7ed7896dbeb4dc2a0b9ac1
Puppy <peraltas4@montclair.edu>
2024-02-27 15:59:02 -0500

    Animal kingdom

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+    public static void main(String[] args){
+    Animal animal = new Animal();
+    Bird bird = new Bird();
+    Cat cat = new Cat();
+    Wolf wolf = new Wolf();
+
+    bird.flee();
+    bird.hunt();
+
+    cat.flee();
+    cat.hunt();
+
+    wolf.flee();
+    wolf.hunt();
+
+
+}
+}
+
+
